10b1a76abdbde93b9642bca423d5ec272237a507
NT <ntan231002@gmail.com>
2026-04-26 18:17:26 +0700

    Add Dialogue Data Script

---
+++ Assets/Scripts/DialogueData.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class DialogueData
+{
+    public string dialogueID;
+    public string characterName;
+    public string dialogueText;
+    public string spriteName;
+    public string nextDialogueID;
+}
